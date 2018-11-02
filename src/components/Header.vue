cbe924b45bd0e9ca0fceea6fd8fb06e5f17f4402
wangxiao <15133676286@163.com>
2018-11-02 23:09:19 +0800

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <header>
+    <div class="item">
+      <slot name="left"><span class="mui-icon mui-icon-undo"></span></slot>
+    </div>
+    <div class="item title">
+      <slot name="title">Micisty</slot>
+    </div>
+    <div class="item">
+      <slot name="right">添加</slot>
+    </div>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: "Header"
+  }
+</script>
+
+<style scoped>
+  @import "../assets/css/reset.less";
+  header {
+    display: flex;
+    display: -webkit-flex;
+    flex-direction: row;
+    /*左右居中*/
+    justify-content: center;
+    /*上下居中*/
+    align-items: center;
+    font-size: 0.16rem;
+    height: 0.5rem;
+  }
+  .item:nth-child(1) {
+    flex-grow: 1;
+  }
+  .item:nth-child(2) {
+    flex-grow: 4;
+  }
+  .item:nth-child(3) {
+    flex-grow: 1;
+  }
+  .title{
+    font-size: .2rem;
+    font-weight: 600;
+  }
+
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div class="borderClass">
+      <img src=""/>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+
+      }
+    },
+    components: {
+
+    }
+  }
+</script>
+<style>
+  .borderClass{
+    border: 1px solid #666;
+    width: 2rem;
+    height: 2rem;
+  }
+  .borderClass img{
+
+  }
+</style>
